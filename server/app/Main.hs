4ea866aaf0ee147ea63d0af38ee464c04ee03af2
Sarah Marshall <sarah@sarahmarshall.name>
2020-06-24 22:47:47 -0700

    Save/load world to JSON file

--- server/app/Main.hs
+++ server/app/Main.hs
@@ -1,18 +1,22 @@
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeApplications #-}
 
 module Main (main) where
 
-import Control.Concurrent
+import Control.Concurrent hiding (threadDelay)
 import Control.Exception
 import Control.Monad
 import Control.Monad.Random
 import Data.Aeson
+import Data.Aeson.Encode.Pretty
 import Data.ByteString (ByteString)
 import Data.ByteString.Builder
 import Data.FileEmbed
 import Data.Function
+import Data.Maybe
 import Data.UUID
 import Destiny.Model
 import Network.HTTP.Types
@@ -21,11 +25,16 @@ import Network.Wai
 import Network.Wai.Handler.Warp
 import Network.Wai.Handler.WebSockets
 import Network.WebSockets
+import System.Directory
 import System.FilePath
+import System.IO.Error
 import System.Signal
 import Text.Printf
+import Time.Units
+import Time.Rational
 
-import qualified Data.ByteString.Char8 as ByteString
+import qualified Data.ByteString.Char8 as BS
+import qualified Data.ByteString.Lazy.Char8 as LBS
 import qualified Data.Text as Text
 
 data State = State
@@ -40,32 +49,65 @@ data Client = Client
 
 main :: IO ()
 main = do
-    stateVar <- newMVar emptyState
+    savedWorld <- readSavedWorld
+    stateVar <- newMVar $ newState $ fromMaybe emptyWorld savedWorld
+    _ <- forkIO $ forever $ saveWorldEvery saveInterval stateVar
+    let settings = serverSettings stateVar
     putStrLn $ printf "Listening on port %d." $ getPort settings
     runSettings settings $ websocketsOr defaultConnectionOptions (webSocketsApp stateVar) httpApp
+  where
+    saveInterval = sec 30
 
-settings :: Settings
-settings = defaultSettings
+serverSettings :: MVar State -> Settings
+serverSettings stateVar = defaultSettings
     & setInstallShutdownHandler installShutdownHandler
     & setGracefulShutdownTimeout (Just 5)
   where
     installShutdownHandler closeSocket = installHandler sigINT $ const $ do
         putStrLn "Shutting down."
+        world <- stateWorld <$> readMVar stateVar
+        saveWorld world
         closeSocket
 
-emptyState :: State
-emptyState = State
-    { stateWorld = emptyWorld
+newState :: World -> State
+newState world = State
+    { stateWorld = world
     , stateClients = []
     }
 
 clientAppDir :: [(FilePath, ByteString)]
 clientAppDir = $(embedDir $ "client" </> "app")
 
+getWorldSavePath :: IO FilePath
+getWorldSavePath = do
+    saveFile <- getXdgDirectory XdgData "destiny"
+    return $ saveFile </> "world.json"
+
+saveWorld :: World -> IO ()
+saveWorld world = do
+    path <- getWorldSavePath
+    writeFile path $ LBS.unpack $ encodePretty' format world
+  where
+    format = defConfig { confIndent = Spaces 2 }
+
+saveWorldEvery :: KnownDivRat unit Microsecond => Time unit -> MVar State -> IO ()
+saveWorldEvery interval stateVar = do
+    threadDelay interval
+    world <- stateWorld <$> readMVar stateVar
+    saveWorld world
+
+readSavedWorld :: IO (Maybe World)
+readSavedWorld = do
+    path <- getWorldSavePath
+    catchIOError (decode <$> LBS.pack <$> readFile path) $ \err ->
+        if isDoesNotExistError err
+        then return Nothing
+        else ioError err
+
 httpApp :: Application
 httpApp request respond = respond $ case rawPathInfo request of
     "/" -> response "index.html"
-    path -> response $ ByteString.unpack $ ByteString.tail path
+    path -> response $ BS.unpack $ BS.tail path
   where
     response path = case lookup path clientAppDir of
         Just content ->
@@ -76,13 +118,15 @@ httpApp request respond = respond $ case rawPathInfo request of
 webSocketsApp :: MVar State -> ServerApp
 webSocketsApp stateVar pending = do
     connection <- acceptRequest pending
-    withPingThread connection 30 (return ()) $ do
+    withPingThread connection (toNum @Second pingInterval) (return ()) $ do
         client <- modifyMVar stateVar $ \state@State { stateWorld = world } -> do
             sendTextData connection $ encode world
             evalRandIO $ addClient connection state
         finally
             (forever $ handleMessage connection stateVar)
             (modifyMVar_ stateVar $ return . removeClient (clientId client))
+  where
+    pingInterval = sec 30
 
 addClient :: RandomGen g => Connection -> State -> Rand g (State, Client)
 addClient connection state@State { stateClients = clients } = do
