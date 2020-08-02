23240c28d283f67d1dffacc51cd410c1eb588d91
Sarah Marshall <sarah@sarahmarshall.name>
2020-08-01 21:29:59 -0700

    Remove the Haskell + Elm implementation

--- server/unix/Destiny/System.hs
+++
@@ -1,6 +0,0 @@
-module Destiny.System (setUserName) where
-
-import System.Posix.User
-
-setUserName :: String -> IO ()
-setUserName name = setUserID =<< userID <$> getUserEntryForName name
--- server/win/Destiny/System.hs
+++
@@ -1,4 +0,0 @@
-module Destiny.System (setUserName) where
-
-setUserName :: String -> IO ()
-setUserName _ = error "setUserName is not supported on Windows."
