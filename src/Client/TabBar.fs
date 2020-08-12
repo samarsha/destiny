4b47263304b689a56bb2f9ff9eb3a3ff01235aa5
Sarah Marshall <sarah@sarahmarshall.name>
2020-08-11 23:57:24 -0700

    Add style for tab bar

--- src/Client/TabBar.fs
+++ src/Client/TabBar.fs
@@ -18,22 +18,22 @@ type 'a Message =
     | RemoveTab of 'a
 
 let private viewInactiveTab model dispatch tab =
-    span [ Class "tab-inactive" ]
-         [ button [ OnClick <| fun _ -> SwitchTab tab |> dispatch ]
-                  [ model.Format tab |> str ]
+    span [ Class "tab tab-inactive"
+           OnClick <| fun _ -> SwitchTab tab |> dispatch ]
+         [ span [ Class "tab-name" ] [ model.Format tab |> str ]
            button [ OnClick <| fun _ -> RemoveTab tab |> dispatch ]
-                  [ icon "X" [] ] ]
+                  [ icon "X" [ Tabler.Size 16 ] ] ]
 
 let private viewActiveTab model dispatch tab =
-    span [ Class "tab-active" ]
+    span [ Class "tab tab-active" ]
          [ input [ OnChange <| fun event -> SetTabName (tab, event.Value) |> dispatch
                    Placeholder <| "Name this " + model.Kind.ToLower ()
                    Value <| upcast model.Format tab ]
            button [ OnClick <| fun _ -> RemoveTab tab |> dispatch ]
-                  [ icon "X" [] ] ]
+                  [ icon "X" [ Tabler.Size 16 ] ] ]
 
 let private viewNewTab model dispatch =
-    button [ Class "tab-add"
+    button [ Class "tab tab-add"
              OnClick <| fun _ -> dispatch AddTab ]
            [ icon "Plus" []
              label [] [ str model.Kind ] ]
@@ -43,5 +43,6 @@ let view model dispatch =
     |> List.map (fun tab ->
            (model, dispatch, tab)
            |||> if Option.contains tab model.Active then viewActiveTab else viewInactiveTab)
-    |> List.add (viewNewTab model dispatch) 
+    |> List.add (viewNewTab model dispatch)
+    |> List.add (span [ Class "tab-excess" ] [])
     |> div [ Class "tabs" ]
